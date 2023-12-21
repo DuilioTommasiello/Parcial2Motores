d901d61b2d3a7c35a5e92bd0efa5efbcb3bc1426
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2023-12-21 19:26:43 -0300

    creacion entera del misil, saludos
    ya se creo el misil

--- Assets/Scripts/Player/MisilMove.cs
+++ Assets/Scripts/Player/MisilMove.cs
@@ -4,4 +4,57 @@ using UnityEngine;
 
 public class MisilMove : MonoBehaviour
 {
+    public float speed = 5f;
+    public int dmg = 40;
+    public misilTracker MT;
+
+    void Start()
+    {
+        GameObject misilTracker = GameObject.Find("misilTracker");
+        if (misilTracker != null)
+        {
+            MT = misilTracker.GetComponent<misilTracker>();
+        }
+        else
+        {
+            Debug.LogError("No se pudo encontrar el objeto misilTracker");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (MT != null && MT.enemyHasBeenFound == true)
+        {
+            if (MT.enemyList.Count > 0 && MT.enemyList[0] != null)
+            {
+                Vector3 targetPos = MT.enemyList[0].transform.position;
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime);
+            }
+            else if (MT.nextEnemy < MT.enemyList.Count && MT.enemyList[MT.nextEnemy] != null)
+            {
+                Vector3 NextTargetPos = MT.enemyList[MT.nextEnemy].transform.position;
+                transform.position = Vector3.MoveTowards(transform.position, NextTargetPos, speed * Time.fixedDeltaTime);
+            }
+
+        }
+        
+    }
+        void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.layer == 6)
+            {
+                Debug.Log("i collition with an enemy");
+                var enemy =collision.gameObject.GetComponent<Enemy>();
+                if (enemy != null)
+                {
+                    enemy.TakeDmg(dmg);
+                    Destroy(gameObject);
+                }
+            }
+            else
+            {
+                Debug.Log("the collition isnt Working " + collision.gameObject.layer);
+            }
+        }
 }
---
+++ Assets/Scripts/Player/misilTracker.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class misilTracker : MonoBehaviour
+{
+    public int nextEnemy;
+    private float alturaincial = 2f;
+    public bool enemyHasBeenFound;
+    public List<GameObject> enemyList = new List<GameObject>();
+    public GameObject misil;
+
+    private void FixedUpdate()
+    {
+        if(transform.position.y < alturaincial)
+        {
+            transform.Translate(Vector2.up * Time.fixedDeltaTime);
+        }       
+        if(misil == null)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 6)
+        {
+            Debug.Log("existe un enemigo en la zona de trackeo ");  
+            enemyHasBeenFound = true;
+            enemyList.Add(collision.gameObject);
+            nextEnemy++;
+        }else
+        {
+            Debug.Log("the collition isnt Working " + collision.gameObject.layer);
+        }
+    }
+}
+
