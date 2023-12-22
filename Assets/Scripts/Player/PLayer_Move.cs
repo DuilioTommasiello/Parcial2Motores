f92144793f16b23b71bd8c21bde358827e0773f6
chispitasMB <107868815+chispitasMB@users.noreply.github.com>
2023-12-22 13:50:19 -0300

    poryecto para entregar
    eso

--- Assets/Scripts/Player/PLayer_Move.cs
+++ Assets/Scripts/Player/PLayer_Move.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Animations;
+using UnityEngine.Animations;
+
+
 
 public class PLayer_Move : Player
 {
